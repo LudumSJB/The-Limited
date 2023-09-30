d9645ea8ce43e6751fc6cf39eb2505a42b1c7246
crare <Crare@users.noreply.github.com>
2023-09-30 14:37:29 +0300

    bigger dialog, dialog text, colors

--- scripts/dialog_system.gd
+++ scripts/dialog_system.gd
@@ -5,19 +5,25 @@ extends Node2D
 @onready var face = $Sprite2D/face
 @onready var face_position_1 = $Sprite2D/facePosition1
 @onready var face_position_0 = $Sprite2D/facePosition0
+@onready var dialog_position_0 = $dialogPosition0
+@onready var dialog_position_1 = $dialogPosition1
+@onready var dialog_position_2 = $dialogPosition2
 
+@export var showDialogAtSceneStart : bool = true
 @export var dialogsPath: String
 @export var faces: Array[Texture]
+
 var current_dialog: int = 0
-#@export() var dialogs: Dictionary
 var dialogs: Array
 
 func _ready():
 	loadDialogs()
+	if showDialogAtSceneStart:
+		showText(dialogs[current_dialog])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):	
-	if Input.is_action_just_pressed("next_text"):
+	if Input.is_action_just_released("next_text"):
 		current_dialog += 1
 		showText(dialogs[current_dialog])
 
@@ -30,21 +36,34 @@ func loadDialogs():
 	if obj == null:
 		print("couldn't parse json!")
 	dialogs = obj
-	print("dialogs: ", dialogs)
-	print("typeof dialogs: ", typeof(dialogs))
 	print(dialogs[0].text)
 
 func hideText():
 	sprite_2d.visible = false
 
 func showText(dialog):
+	# show new text in dialog
 	sprite_2d.visible = true
 	rich_text_label.clear()
 	rich_text_label.append_text(dialog.text)
-	face.texture = faces[dialog.face]
-	if dialog.position == 0:
-		sprite_2d.flip_h = false
-		face.position = face_position_0.position
+	
+	# move dialog box position
+	if dialog.dialog_position == 0:
+		sprite_2d.position = dialog_position_0.position
+	elif dialog.dialog_position == 1:
+		sprite_2d.position = dialog_position_1.position
+	elif dialog.dialog_position == 2:
+		sprite_2d.position = dialog_position_2.position
+	
+	# change face and its position relative to dialog box
+	if dialog.face != -1:
+		face.visible = true
+		face.texture = faces[dialog.face]
+		if dialog.position == 0:
+			sprite_2d.flip_h = false
+			face.position = face_position_0.position
+		else:
+			sprite_2d.flip_h = true
+			face.position = face_position_1.position
 	else:
-		sprite_2d.flip_h = true
-		face.position = face_position_1.position
+		face.visible = false
