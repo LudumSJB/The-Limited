8d1da1964a929d3b255f7e3ad7bd478a76990a25
sso <93847254+ssoq@users.noreply.github.com>
2023-09-30 11:08:32 +0100

    Timing Accuracy Bar
    Implemented timing accuracy bar along with signals which are emitted when the player hits accurately or misses

---
+++ Scripts/AreaHitter.gd
@@ -0,0 +1,39 @@
+extends Area2D
+
+@export var canHit = false
+
+signal playerHit
+signal playerMiss
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	check_if_player_can_hit()
+	pass
+	
+func check_if_player_can_hit():
+	if canHit == true and Input.is_action_just_pressed("action_button"): # checks for when the player is able to hit and if they do hit
+		playerHit.emit()
+	elif canHit == false and Input.is_action_just_pressed("action_button"): # checks for when the player is not able to hit and if they still do hit
+		playerMiss.emit()
+
+func player_on_point(area):
+	if area.name == "HitArea": # checks if the current collision area is a hittable area
+		canHit = true
+	pass # Replace with function body.
+
+
+func player_off_point(area):
+	if area.name == "HitArea": # checks if the current collision area is a non-hittable area
+		canHit = false
+	pass # Replace with function body.
+	
+func player_on_time():
+	print("On Time!")
+	
+func player_not_on_time():
+	print("You Fucked Up!")
---
+++ Scripts/AreaHitterPath.gd
@@ -0,0 +1,14 @@
+extends PathFollow2D
+
+@export var speed = 0.2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	progress += speed + delta
+	pass
