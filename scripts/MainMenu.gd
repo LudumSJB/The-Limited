492aca10e71173a9ec45cea7791e0a70add9ef59
crare <Crare@users.noreply.github.com>
2023-10-01 20:26:08 +0300

    more dialog and back to start button to credits and menu to have credts button

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -7,15 +7,13 @@ func _ready():
 		quit_button.visible = false
 
 
-func _process(_delta):
-	pass
-
-
 func play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/root.tscn")
-	pass # Replace with function body.
 
 
 func quit_button_pressed():
 	get_tree().quit()
-	pass # Replace with function body.
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/level8.tscn")
