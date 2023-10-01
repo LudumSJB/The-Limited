76ae24a552a11332f94745f82b1a0afc5a5d1a6b
crare <Crare@users.noreply.github.com>
2023-10-01 18:09:40 +0300

    toilet act

--- scripts/level5.gd
+++ scripts/level5.gd
@@ -1,17 +1,26 @@
 extends Node2D
 
 @onready var dialog_system = $dialogSystem
+@onready var timing_accuracy_bar = $TimingAccuracyBar
+@onready var action_area = $actionArea
+
+signal play_minigame
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_play_minigame():
+	timing_accuracy_bar.activateMinigame()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+func _on_timing_accuracy_bar_minigame_won():
+	timing_accuracy_bar.deactivateMinigame()
+	dialog_system.nextText()
 
 func _on_dancer_dude_dude_go_out():
+	dialog_system.nextText()
+	action_area.monitoring = true
+
+
+func _on_action_area_on_action_initated():
+	# go to toilet
+	action_area.monitoring = false
+	print("_on_action_area_on_action_initated")
 	dialog_system.nextText()
